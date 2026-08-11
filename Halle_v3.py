56d62d4ec1d33458130360e6cb38df595ec00fe2
f4tihklc <fatihkilic1@hotmail.com>
2026-08-11 10:04:07 +0200

    Halle_v3.py

--- Halle_v3.py
+++ Halle_v3.py
@@ -261,8 +261,8 @@ if daten_geladen:
             c1, c2, c3 = st.columns([2, 1, 1])
             c1.markdown(f"<div style='padding-top: 10px;'><b>{team}</b></div>", unsafe_allow_html=True)
             
-            def_t1 = 90 if team == "G-Jugend (Bambini)" else 60
-            def_t2 = 60
+            def_t1 = 60 if team == "G-Jugend (Bambini)" else 90
+            def_t2 = 90
             
             t1_key = f"t1_d_{team}"
             if t1_key not in st.session_state: st.session_state[t1_key] = def_t1
